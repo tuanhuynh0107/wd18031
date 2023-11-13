1cfaa5039697e689afd464b0b44412fcb28b2b6f
tuanhuynh0107 <ht24430@gmail.com>
2023-11-13 23:34:00 +0700

    code php

--- index.php
+++ index.php
@@ -7,32 +7,17 @@
         $page = $_GET['page'];
 
         switch ($page) {
-            case 'product':
-                require_once "view/product.php";
-                break;
-            case 'cart':
-                require_once "view/cart.php";
-                break;
-            case 'payMent':
-                require_once "view/payMent.php";
-                break;
             case 'login':
                 require_once "view/login.php";
                 break;
-            case 'sign':
+            case 'register':
                 require_once "view/sign.php";
                 break;
-            case 'admin':
-                require_once "admin/view/index.php";
-                break;
             default:
-
-            $product = getProduct();
                 require_once "view/home.php";
                 break;
         };
     }else{
-        $product = getProduct();
         require_once "view/home.php";
     }
     require_once "view/footer.php";
